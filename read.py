a5495341a4e6003f1624c27298ecc8e1d69976c5
Rax Chen <rockpose904@gmail.com>
2019-07-29 13:56:29 +0800

    add commit

--- read.py
+++ read.py
@@ -10,9 +10,10 @@ with open('reviews.txt', 'r') as f:
 			print(len(data)) #目前讀取進度
 print('檔案讀取完畢，總共有', len(data), '資料')
 
+#計算每篇留言的平均字數
 read_len = 0
 for d in data:
 	i = len(d)
-	read_len = read_len + i
-print ('留言的平均長度為' , read_len / len(data) ,'字')
+	read_len = read_len + i #將每則留言加總
+print ('留言的平均長度為' , read_len / len(data) ,'字') #去除以總筆數
 
